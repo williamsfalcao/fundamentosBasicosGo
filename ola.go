674829def71ddd44403dff3c97f5b72ed78c853e
Williams Machado <williams.machado@brq.com>
2023-05-17 17:11:49 -0300

    first commit

---
+++ ola.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+const prefixoOlaPortugues = "Olá, "
+
+func Ola(nome string) string {
+	if nome == "" {
+		nome = "Mundo"
+	}
+	return prefixoOlaPortugues + nome
+}
+
+func main() {
+	fmt.Println(Ola("mundo"))
+}
